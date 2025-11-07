301e9aeb46c340f222a9620c2a074f3be03d7193
yzSant <lcssantana1805@gmail.com>
2025-11-07 00:35:24 -0300

    feat-menu-portfolio

--- src/Layout.jsx
+++ src/Layout.jsx
@@ -1,12 +1,14 @@
+import Btn from "./components/Btn";
 import Navbar from "./components/Navbar";
 import { Outlet } from "react-router-dom";
 
 export default function Layout() {
   return (
-    <div className="min-h-screen bg-gray-100">
+    <div className="min-h-screen flex bg-[#EAF0FF]">
       <Navbar />
-      <main>
+      <main className="flex-1 p-6 bg-[#212529] text-[#F9FAFB]">
         <Outlet />
+        <Btn/>
       </main>
     </div>
   );
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,52 +1,11 @@
-import { useEffect, useState } from "react";
 import ProdCard from "../components/ProdCard";
 import ProdContainer from "../components/ProdContainer";
 
 export default function Home() {
-  const [products, setProducts] = useState([]);
-  const [title, setTitle] = useState([]);
-  const [image, setImage] = useState([]);
-  const [price, setPrice] = useState([]);
-  const API = import.meta.env.VITE_API_URL;
-
-  useEffect(() => {
-    fetch(`${API}products/`)
-      .then(res => res.json())
-      .then(data => setProducts(data))
-      .catch(err => console.error("Erro ao buscar produtos:", err));
-
-    fetch(`${API}products/`)
-      .then(res => res.json())
-      .then(data => { setPrice(data.price) })
-      .catch(err => console.error(err));
-
-    fetch(`${API}products/`)
-      .then(res => res.json())
-      .then(data => { setTitle(data.title) })
-      .catch(err => console.error(err));
-
-    fetch(`${API}products/`)
-      .then(res => res.json())
-      .then(data => { setImage(data.image) })
-      .catch(err => console.error(err));
-    }, [API]);
-
-  console.log(setPrice);
-
   return (
     <div>
       <h1 className="text-3xl font-bold text-center mb-8">Nossos Produtos</h1>
-
       <ProdContainer title="Produtos de destaque">
-        <>
-        {products.map(product => (
-          <ProdCard
-            name={product.title}
-            price={product.price}
-            image={product.image}
-          />
-        ))}
-        </>
       </ProdContainer>
     </div>
   );
